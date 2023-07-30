53fe7b85deb69474b1d28563528a2f04fd7d05fc
Mhd Jawad Al Rahwanji <mhal00002@stud.uni-saarland.de>
2023-07-30 04:18:04 +0200

    - Reformat knn_clf_score.py

--- src/knn_clf_score.py
+++ src/knn_clf_score.py
@@ -2,32 +2,31 @@ import torch
 from tqdm.notebook import tqdm
 import torch.nn.functional as F
 
+
 @torch.no_grad()
-def extract_ds_features(model, data_loader, get_features_fn,  device):
-    '''
-    pass the torch model and the dataloader along with the get_img_features function 
-    '''
+def extract_ds_features(model, data_loader, get_features_fn, device):
+    """
+    pass the torch model and the dataloader along with the get_img_features function
+    """
     feature_list, labels_list = [], []
     for batch in tqdm(data_loader, leave=False):
         img_tensor, labels = batch[0].to(device), batch[1].to(device)
         feature_tensor = get_features_fn(model, img_tensor)
         feature_list.append(feature_tensor)
         labels_list.append(labels)
-    
+
     all_features_tensor = torch.cat(feature_list)
     all_labels_tensor = torch.cat(labels_list)
 
     return all_features_tensor, all_labels_tensor
-    
-    
 
 
 @torch.no_grad()
 def knn_classifier(train_features, train_labels, test_features, test_labels, k=5, num_classes=10):
-    '''
+    """
     pass train features and labels to be same as test if we don't have a train set features
     returns:  top1, top5 accuracy
-    '''
+    """
     top1, top5, total = 0.0, 0.0, 0
     train_features = train_features.t()
     # print(train_features.shape)
@@ -37,7 +36,7 @@ def knn_classifier(train_features, train_labels, test_features, test_labels, k=5
     all_idxs = torch.arange(test_labels.shape[0])
     for idx in tqdm(range(0, num_test_images, imgs_per_chunk), leave=False):
         # get the features for test images
-        si, ei =  idx, min((idx + imgs_per_chunk), num_test_images)
+        si, ei = idx, min((idx + imgs_per_chunk), num_test_images)
         ixs = (torch.arange(si, ei))
         # mask = all_idxs[all_idxs!=ixs]
         index = torch.ones(all_idxs.shape[0], dtype=bool)
@@ -52,7 +51,7 @@ def knn_classifier(train_features, train_labels, test_features, test_labels, k=5
         train_features_f = train_features
         # calculate the dot product and compute top-k neighbors
         similarity = torch.mm(features, train_features_f)
-        distances, indices = similarity.topk(k+1, largest=True, sorted=True)
+        distances, indices = similarity.topk(k + 1, largest=True, sorted=True)
         distances, indices = distances[:, 1:], indices[:, 1:]
         # print(distances.shape, indices.shape)
         candidates = train_labels.view(1, -1).expand(batch_size, -1)
@@ -73,12 +72,13 @@ def knn_classifier(train_features, train_labels, test_features, test_labels, k=5
             1,
         )
         # print(probs)
-        _, predictions = probs.sort(1, True) #torch.mode(retrieved_neighbors).values#probs.sort(1, True)
-    
+        _, predictions = probs.sort(1, True)  # torch.mode(retrieved_neighbors).values#probs.sort(1, True)
+
         # find the predictions that match the target
         correct = predictions.eq(targets.data.view(-1, 1))
         # print(correct.narrow(1, 0, 1).sum().item())
-        top1 = top1 + correct.narrow(1, 0, 1).sum().item() #(predictions==targets).sum().item() #correct.narrow(1, 0, 1).sum().item()
+        top1 = top1 + correct.narrow(1, 0,
+                                     1).sum().item()  # (predictions==targets).sum().item() #correct.narrow(1, 0, 1).sum().item()
         top5 = top5 + correct.narrow(1, 0, min(5, k)).sum().item()  # top5 does not make sense if k < 5
         total += targets.size(0)
     top1 = top1 * 100.0 / total
